de146bf6041d77d8e791492f0c6a02931594a170
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-09-22 21:46:57 +0600

    leetcode 1st problem solving

--- easy/twoSum.py
+++ easy/twoSum.py
@@ -1,7 +1,11 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length = len(nums)
-        i = 0
-        while i<=length:
-            if nums[i] + nums[i+1] == target:
-                return [i, i+1]
-            i += 1
+def twoSum(nums: List[int], target: int) -> List[int]:
+    length = len(nums)
+    i = 0
+    while i < length:
+        j = i+1
+        while j < length:
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            j += 1
+        i += 1
+        # j += 1 
