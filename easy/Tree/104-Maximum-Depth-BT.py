35a9ae75d8b60eae3fa45ae5530cc1ac22716140
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-11-04 22:49:39 +0600

    solved Maximum Depth Binary Tree - DFS

--- easy/Tree/104-Maximum-Depth-BT.py
+++ easy/Tree/104-Maximum-Depth-BT.py
@@ -30,3 +30,21 @@
 #         elif root.left is None and root.right is None:
 #             return 1
         
+class Solution:
+    
+    def recurseDFS(self, node, level):
+        if node is not None:
+            right,left = level, level
+            if node.left is not None:
+                left = self.recurseDFS(node.left, level+1)
+            if node.right is not None:
+                right = self.recurseDFS(node.right, level+1)
+            return max(left, right)
+                        
+
+    def maxDepth(self, root):
+        node = root
+        if node is not None:
+            return self.recurseDFS(node, 1)
+        else:
+            return 0
